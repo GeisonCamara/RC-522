6186a2e105e102dbdb200434b9241225f647d642
geisoncamara <geison.camara@hotmail.com>
2018-02-23 14:18:30 -0300

    Ajuste servidor

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+var http = require('http');
+var port = 5000;
+var ip = '192.168.1.119';
 var rc522 = require("rc522");
 
 console.log("Pronto!");
 
 rc522(function(rfidSerialNumber){
     console.log(rfidSerialNumber);
+});
+
+var server = http.createServer(function(req, res){
+    res.end('RF-522');
+});
+
+server.listen(port, id, function(){
+    console.log('Servidor iniciado em http://${id}:${port}')
+    console.log('Para finalizar o servidor pressione CTRL+C');
 });
