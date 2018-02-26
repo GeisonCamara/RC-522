f3919d31de32d40e43b74a99d5d6c0881491a9d6
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-02-26 14:38:12 -0300

    Banco de dados

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 var port = 5000;
-var ip = '192.168.1.119';
+var ip = 'localhost';
 var rc522 = require("rc522");
-
+var jsonfile = require('jsonfile');
+var db = './database/db.json';
 var id = 0;
+var i = 0;
+
 console.log("Pronto!");
 
 rc522(function(rfidSerialNumber){
@@ -12,7 +15,19 @@ rc522(function(rfidSerialNumber){
 });
 
 var server = http.createServer(function(req, res){
-    res.end('RFID: ' + id);
+    jsonfile.readFile(db, function(err, data){
+    if(err){
+        throw err;
+    }
+    else{
+        for(i = 0; i < data.users.length; i++){
+            if(id == data.users[i].rfid){
+                console.log(data.users[i].nome);
+                res.end('RFID: ' + data.users[i].nome);
+            }
+        }
+    }
+    });
 });
 
 server.listen(port, ip, function(){
