2cbfc9818f342f948523593c5beb3aef83bda075
Thiago Maldaner <thiago.maldaner@digitaldesk.com.br>
2018-02-28 11:20:31 -0300

    adicionado handlebars, express, nodemon e a lógica de inicialização do servidor

--- server.js
+++ server.js
@@ -1,54 +1,49 @@
-'use strict';
-var http = require('http');
+var express = require('express');
+var exphbs = require('express-handlebars');
 var port = 5000;
-var ip = '192.168.1.119';
-var io = require('socket.io')(server);
 var rc522 = require("rc522");
 var jsonfile = require('jsonfile');
 var db = './database/db.json';
 var id = "90152629";
-var i = 0;
-var show;
-var connectCounter = 0;
-var kk;
-var count = 1;
 
-console.log("Pronto!");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-rc522(function(rfidSerialNumber){
-    console.log(rfidSerialNumber);
-    id = rfidSerialNumber;
-});
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 function checkUsers(data, rfid){
-    for(i = 0; i < data.users.length; i++){
+    for(var i = 0; i < data.users.length; i++){
         if(id == data.users[i].rfid){
             return data.users[i].nome;
         }
     }
 }
 
-var server = http.createServer(function(req, res){
+app.get('/', function(req, res){
     jsonfile.readFile(db, function(err, data){
         if(err) throw err;
         var name = checkUsers(data, id);
         console.log(name);
         if(name != undefined){
-            res.end('RFID: ' + name);
+            res.render('index', {rfid: name});
         } else {
-            res.end('RFID não cadastrado');
+            res.render('index', {rfid: 'Não encontrado'});
         }
     });
 });
 
+rc522(function(rfidSerialNumber){
+    console.log(rfidSerialNumber);
+    id = rfidSerialNumber;
+    res.redirect('/');
+});
+
 io.on('connection', function(socket){
     console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-  });
+});
 
-server.listen(port, ip, function(){
-    console.log('Servidor iniciado em http://' + ip + ':' + port);
-    console.log('Para finalizar o servidor pressione CTRL+C');
+http.listen(port, function(){
+    console.log('Servidor iniciado: http://localhost:' + port);
 });
