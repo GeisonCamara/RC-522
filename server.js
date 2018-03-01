f3a5b1ef32167af768436763fc770d64d67c298c
Geison Camara <geison.camara@digitaldesk.com.br>
2018-03-01 10:14:09 -0300

    Revert manual

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
-var http = require('http');
 var express = require('express');
-var app = express();
-var exphbs = require('express3-handlebars');var path = require('path');
-var server = http.createServer(app);
+var exphbs = require('express-handlebars');
 var port = 5000;
-var io = require('socket.io').listen(server);
+var rc522 = require("rc522");
+var jsonfile = require('jsonfile');
+var db = './database/db.json';
+var id = "90152629";
 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-
-
-
-
+function checkUsers(data, rfid){
+    for(var i = 0; i < data.users.length; i++){
+        if(data.users[i].rfid == rfid){
+            return data.users[i].nome;
+        }
+    }
 }
 
-
+app.get('/', function(req, res){
+    rc522(function(rfidSerialNumber){
+        jsonfile.readFile(db, function(err, data){
+            if(err) throw err;
+            var name = checkUsers(data, rfidSerialNumber);
+            console.log(name);
+            if(name != undefined){
+                res.render('index', {rfid: name});
+            } else {
+                res.render('index', {rfid: 'Não encontrado'});
+            }
+        });
+    });
 });
 
-
-io.sockets.on('connection', function(socket){
-    console.log('Um usuário se conectou!');
-    socket.on('disconnect', function(){
-        console.log('Um usuário se desconectou!');
-    });
-    socket.on('read rfid', function(rfid){
-        console.log('Última leitura: ' + rfid);
-    });
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
-
-
-
-
+http.listen(port, function(){
+    console.log('Servidor iniciado: http://localhost:' + port);
 });
