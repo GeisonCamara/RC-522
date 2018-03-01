b8a82bc9cefbb718ff0c645ced91513396ca0934
Geison Camara <geison.camara@digitaldesk.com.br>
2018-03-01 10:03:15 -0300

    Revert "require express"
    This reverts commit d59f44c80eb2e38c2c859b211ae10362c6362b5a.
    
    # Conflicts:
    #	server.js

--- server.js
+++ server.js
@@ -1,67 +1,22 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var path = require('path');
+var exphbs = require('express3-handlebars');var path = require('path');
 var server = http.createServer(app);
 var port = 5000;
 var io = require('socket.io').listen(server);
-var rfid = require('./controllers/rfid');
-var helpers = require('./helpers/helpers');
 
-var hbs = exphbs.create({
-    defaultLayout: 'main',
-    helpers: helpers
-});
 
-hbs.getTemplates('views/partials/', {
-    cache: app.enabled('view cache'),
-    precompiled: true
-}).then(function(templates){
-    var extRegex = new RegExp(hbs.extname, '$');
-    var partials = {};
-    Object.keys(templates).map(function(name){
-        partials[name.replace(extRegex, '')] = templates[name];
-        return partials;
-    });
-    if (templates.length) {
-        hbs.handlebars.partials = partials;
-    }
-});
 
-var index = require('./routes/index');
 
-app.use(logger('common'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(compress());
-app.use(helpers.relativePath() + '/content', express.static(path.join(__dirname, '..', 'content')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
-if (app.get('env') !== 'development') {
-    app.enable('view cache');
+
+
 }
 
-app.use(helpers.relativePath() + '/', index);
 
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
 });
 
-app.use((error, request, response) => {
-    // set locals, only providing error in development
-    const err = error;
-    const req = request;
-    const res = response;
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 io.sockets.on('connection', function(socket){
     console.log('Um usuário se conectou!');
@@ -73,12 +28,8 @@ io.sockets.on('connection', function(socket){
     });
 });
 
-app.use(rfid);
 
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), () => {
-  console.log(`WebService has started on ${app.get('port')} running in ${app.get('env')} mode`);
-  if (app.get('env') === 'development') {
-    console.log('PLEASE NOTE: your webservice is running not in a production mode!');
-  }
+
+
+
 });
