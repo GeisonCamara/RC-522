3184d7799404c4f71e511934e011668f698e2d07
geisoncamara <geison.camara@hotmail.com>
2018-03-01 11:02:29 -0300

    lógica do for

--- server.js
+++ server.js
@@ -16,23 +16,12 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 function checkUsers(data, rfid){
-    console.log('_______________________________________');
-    console.log('3 - data e rfid');
-    console.log(data);
-    console.log(rfid);
-    console.log('_______________________________________');
     for(var i = 0; i < data.users.length ; i++){
-        console.log(i);
-        console.log(data.users.length);
-        // console.log('_______________________________________');
-        // console.log('4 - itens');
-        // console.log(i + ': ' + data.users[i].rfid);
-        // console.log('_______________________________________');
-        // if(data.users[i].rfid == rfid){
-        //     return data.users[i].nome;
-        // } else {
-        //     return 'Não encontrado';
-        // }
+        if(data.users[i].rfid == rfid){
+            return data.users[i].nome;
+        }
+
+        return 'não encontrado';
     }
 }
 
@@ -48,22 +37,10 @@ io.on('connection', function(socket){
 });
 
 rc522(function(rfidSerialNumber){
-    console.log('_______________________________________');
-    console.log('1 - rfidSerialNumber: ' + rfidSerialNumber);
-    console.log('_______________________________________');
     jsonfile.readFile(db, function(err, data){
-        console.log('_______________________________________');
-        console.log('2 - Data: ');
-        console.log(data);
-        console.log('_______________________________________');
         if(err) throw err;
         id = rfidSerialNumber;
         msg = checkUsers(data, id);
-        console.log('_______________________________________');
-        console.log('5 - id e msg');
-        console.log(id);
-        console.log(msg);
-        console.log('_______________________________________');
     });
 });
 
