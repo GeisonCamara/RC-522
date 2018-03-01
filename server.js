cf4b70bd4918c4d2ccb61075ac9cba3851c5003c
Geison Camara <geison.camara@digitaldesk.com.br>
2018-03-01 10:54:01 -0300

    teste lógico

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 function checkUsers(data, rfid){
+    console.log('_______________________________________');
+    console.log('3 - data e rfid');
     console.log(data);
     console.log(rfid);
+    console.log('_______________________________________');
     for(var i = 0; i < data.users.length; i++){
-        console.log(data.users[i].rfid);
+        console.log('_______________________________________');
+        console.log('4 - itens');
+        console.log(i + ': ' + data.users[i].rfid);
+        console.log('_______________________________________');
         if(data.users[i].rfid == rfid){
             return data.users[i].nome;
         } else {
@@ -40,11 +46,19 @@ io.on('connection', function(socket){
 });
 
 rc522(function(rfidSerialNumber){
+    console.log('_______________________________________');
+    console.log('1 - rfidSerialNumber: ' + rfidSerialNumber);
+    console.log('_______________________________________');
     jsonfile.readFile(db, function(err, data){
+        console.log('_______________________________________');
+        console.log('2 - Data: ');
+        console.log(data);
+        console.log('_______________________________________');
         if(err) throw err;
         id = rfidSerialNumber;
         msg = checkUsers(data, id);
         console.log('_______________________________________');
+        console.log('5 - id e msg');
         console.log(id);
         console.log(msg);
         console.log('_______________________________________');
