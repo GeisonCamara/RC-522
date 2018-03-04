594be71763ddb3001b78502c59eb6a6ef358f561
geisoncamara <geison.camara@hotmail.com>
2018-03-03 23:16:30 -0300

    Path notifyjs and jquery

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,20 @@
+'use strict';
+const relativePath = process.env.RELATIVE_PATH || '';
+
 module.exports = {
-    io:function(){
-                
+    relativePath: () => {
+        return relativePath;
+    },
+    resource: (path) => {
+        return relativePath + '/content' + path;
+    },
+    npm:(path) =>{
+        return relativePath + '/node_modules' + path;
+    },
+    url: function (path) {
+        return relativePath + path;
+    },
+    toLowerCase: function (word){
+        return word.toLowerCase();
     }
-}
+};
