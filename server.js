ad9fec5c08f5a01268aa5f4cbcee45345e8165f5
geisoncamara <geison.camara@hotmail.com>
2018-03-04 00:03:37 -0300

    Paths

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ var io = require('socket.io').listen(server);
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use('/jquery', express.static(path.resolve(__dirname, 'content')));
+app.use('/content', express.static(path.resolve(__dirname, 'content')));
 // app.use('/notifyjs', express.static(path.join(__dirname, 'node_modules/notifyjs/dist/')));
-console.log(path.join(__dirname, 'node_modules/notifyjs/dist/'));
 
 function checkUsers(data, rfid){
     var userName = "";
