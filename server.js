dfdb7a54cc7feb2b8c7b1318cf019614ba4e9737
geisoncamara <geison.camara@hotmail.com>
2018-03-04 10:26:13 -0300

    Paths

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var io = require('socket.io').listen(server);
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-console.log(path.join(__dirname, 'content'));
+
 app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/notify', express.static(path.join(__dirname, '/node_modules/notifyjs/dist')));
 
